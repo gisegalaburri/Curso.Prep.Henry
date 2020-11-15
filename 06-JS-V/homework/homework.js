50e99d494079a6490f8fe8c6f761693c9a5d8193
Gisele Galaburri <gisele.galaburri89@gmail.com>
2020-11-15 04:28:50 -0300

    homework 5 completed

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -4,10 +4,38 @@ function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
   // En el `contructor`, define el usuario, el nombre, el email y la contraseña
-  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 'Hola, mi nombre es {{nombre}}'
+  // El `contructor` debe tener un método llamado "saludar" en su `prototype` que devuelva una string 
+  //'Hola, mi nombre es {{nombre}}'
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  
+  //Ambas formas de crear usuarios funcionan igual
+  class Usuario {
+    constructor  (opciones) {
+      this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+    }
+
+    saludar() {
+      return "Hola, mi nombre es " + this.nombre;
+    }
+  }
+
+/*   function Usuario (opciones) {
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.password = opciones.password;
+  }
+
+  Usuario.prototype.saludar = function() {
+    return "Hola, mi nombre es " + this.nombre;
+  } */
+
+  return Usuario;
 
 }
 
@@ -15,6 +43,10 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+  Constructor.prototype.saludar = function() {
+    return "Hello World!";
+  }
 
 }
 
@@ -24,6 +56,15 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+
+  String.prototype.reverse = function() {
+      var strReversed = '';
+      for (var i = this.length - 1; i >= 0; i--) {
+        strReversed += this.charAt(i);
+      }
+      return strReversed;
+  }
+  
 
 }
 
